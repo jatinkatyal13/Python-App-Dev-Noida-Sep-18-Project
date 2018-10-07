019f53750d61857cba035b4e25af47a9af85c757
Jatin Katyal <jatin.katyal13@gmail.com>
2018-10-07 13:03:04 +0530

    changed db

--- quickr/settings.py
+++ quickr/settings.py
@@ -69,17 +69,9 @@ WSGI_APPLICATION = 'quickr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default = config('DATABASE_URL'))
-    }
+DATABASES = {
+    'default': dj_database_url.config(default = config('DATABASE_URL'))
+}
 
 
 # Password validation
